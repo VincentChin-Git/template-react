894016419dffa3975544c8ecbf8425016d1a2864
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2023-10-24 13:12:47 +0800

    chore: init template

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+const configs = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "button",
+      ],
+    ],
+  },
+};
+
+export default configs;
